1a66e946d37408264e54b0898ae408166987de8f
Raj Panchal <panchalraj512@gmail.com>
2025-09-14 10:10:45 +0530

    setup initial Angular project structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './auth/components/login/login.component';
+import { AuthEnum } from './enum/auth.enum';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+      {
+            path: AuthEnum.LOGIN,
+            component: LoginComponent,
+            pathMatch: 'full',
+            title: 'Log In'
+      }
+];
---
+++ src/app/enum/auth.enum.ts
@@ -0,0 +1,3 @@
+export enum AuthEnum {
+      LOGIN = 'log-in'
+}
