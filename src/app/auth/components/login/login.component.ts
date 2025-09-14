35bf301da695ff5a716ddb75e93fe080965e3f5f
Raj Panchal <panchalraj512@gmail.com>
2025-09-14 16:55:59 +0530

    Changes done for login flow

--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  logInForm!: FormGroup;
+  signUpForm!: FormGroup;
+  isLogInFormVisible: boolean = true;
 
+  readonly fb = inject(FormBuilder);
+
+  ngOnInit(): void {
+    this.buildForm();
+  }
+
+  buildForm(): void {
+    this.buildLogInForm();
+    this.buildSignUpForm();
+  }
+
+  buildLogInForm(): void {
+    this.logInForm = this.fb.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    })
+  }
+
+  buildSignUpForm(): void {
+    this.signUpForm = this.fb.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    })
+  }
+
+  get toggleSignUpForm(): boolean {
+    return this.isLogInFormVisible = !this.isLogInFormVisible;
+  }
 }
